749218a5baf822731c8bf3c1e085f98ca8cd7124
Stéphane Lavergne <lis@imars.com>
2019-02-23 09:14:00 -0500

    Old changes

--- openvpn-namespace.sh
+++ openvpn-namespace.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
-# Invoke with:
-#
-# ifconfig-noexec
-# route-noexec
-# up /etc/openvpn/openvpn-namespace.sh
-# route-up /etc/openvpn/openvpn-namespace.sh
-# down /etc/openvpn/openvpn-namespace.sh
-#
-# Will create a new kernel network namespace named like the basename of your
-# OpenVPN config file.  (Watch out: renaming such files on Debian requires
-# running "systemctl daemon-reload" to take effect!)
-
-# Inspired from: http://www.naju.se/articles/openvpn-netns.html
+# Inspired from http://www.naju.se/articles/openvpn-netns.html
 # ...but it didn't work out of the box (interfaces set up but no I/O went through)
 #
 # He forgot to set $5 as the point-to-point remote end.
 #
-# Also, the DHCP DNS portion is adapted from:
-# https://github.com/masterkorp/openvpn-update-resolv-conf/blob/master/update-resolv-conf.sh
+# DHCP DNS portion from https://github.com/masterkorp/openvpn-update-resolv-conf/blob/master/update-resolv-conf.sh
+#
+# The new netns will be called by the basename of your OpenVPN configuration file.
+# (Watch out: renaming such a file on Debian requires running "systemctl daemon-reload"!)
 
 NS=$(basename -s .conf $config)
 NETNSDIR="/etc/netns/$NS"
@@ -41,7 +31,7 @@ case $script_type in
 		# echo -e "nameserver 8.8.8.8\n8.8.4.4" >>${RESOLVCONF}
 
 		ip link set dev "$1" up netns $NS mtu "$2"
-		ip netns exec $NS ip addr add dev "$1" "$4/${ifconfig_netmask:-30}" peer "$5" ${ifconfig_broadcast:+broadcast "$ifconfig_broadcast"}
+		ip netns exec $NS ip addr add dev "$1" "$4/${ifconfig_netmask:-30}" ${ifconfig_remote:+peer "$ifconfig_remote"} ${ifconfig_broadcast:+broadcast "$ifconfig_broadcast"}
 		test -n "$ifconfig_ipv6_local" && ip netns exec $NS ip addr add dev "$1" "$ifconfig_ipv6_local"/112
 	;;
 
@@ -51,7 +41,8 @@ case $script_type in
 	;;
 
 	down)
-		ip netns delete $NS
+		#ip netns delete $NS
+		ip netns exec $NS ip route del default
 	;;
 
 esac
